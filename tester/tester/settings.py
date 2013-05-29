8568a41b721101f44d05bf7d11e2c0b372dab95a
jazzylemur <jazzylemur@gmail.com>
2013-05-29 22:48:33 +0530

    Update settings.py

--- tester/tester/settings.py
+++ tester/tester/settings.py
@@ -107,19 +107,11 @@ ROOT_URLCONF = 'tester.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'tester.wsgi.application'
-#PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
+PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
 
-# here() gives us file paths from the root of the system to the directory
-# holding the current file.
-here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
-
-PROJECT_ROOT = here("..")
-# root() gives us file paths from the root of the system to whatever
-# folder(s) we pass it starting at the parent directory of the current file.
-root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
 
 TEMPLATE_DIRS = ( 
-    root(pools),
+    os.path.join(PROJECT_DIR, "pools"),,
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
