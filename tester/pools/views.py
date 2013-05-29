2dcf8b5ece58bddc465bc907ce89bd23cad98cca
jazzylemur <jazzylemur@gmail.com>
2013-05-29 23:16:22 +0530

    Update views.py

--- tester/pools/views.py
+++ tester/pools/views.py
@@ -5,7 +5,9 @@ from django.template import *
 from tester import *
 
 def index(request):
-    return render_to_response("one.html",RequestContext(request))
+    o=Template(" <style type=""text/css""> #tb{    	text-align:center;	}  <div>    <div>	<p>	<div id=""tb""> TextLock </div></p> </div> <div><p id=""tb""> Enter text to be matched : </p> <form action=""two.html"" method=""POST""> {% csrf_token %}		<div id=""tb""> <textarea cols=""80"" rows=""20"" name=""data""></textarea></div><p><input type=""submit""></p>	</div>
+    disp=o.render()
+    return render_to_response(disp)
 def two(request):
      data = request.POST.get('data', '')
      if(data):
@@ -19,4 +21,5 @@ def two(request):
      res= t.render(c)
      return HttpResponse(res)
      
+    
     
