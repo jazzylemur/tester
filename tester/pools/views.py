0fb4d46c367bf12d4320816a265ec350a8167ab8
jazzylemur <jazzylemur@gmail.com>
2013-05-30 10:02:55 +0530

    Update views.py

--- tester/pools/views.py
+++ tester/pools/views.py
@@ -5,9 +5,10 @@ from django.template import *
 from tester import *
 
 def index(request):
-    o=Template(" <style type=""text/css""> #tb{    	text-align:center;	}  <div>    <div>	<p>	<div id=""tb""> TextLock </div></p> </div> <div><p id=""tb""> Enter text to be matched : </p> <form action=""two.html"" method=""POST""> {% csrf_token %}		<div id=""tb""> <textarea cols=""80"" rows=""20"" name=""data""></textarea></div><p><input type=""submit""></p>	</div>
-    disp=o.render()
-    return render_to_response(disp)
+  #  o=Template(" <style type=""text/css""> #tb{    	text-align:center;	}  <div>    <div>	<p>	<div id=""tb""> TextLock </div></p> </div> <div><p id=""tb""> Enter text to be matched : </p> <form action=""two.html"" method=""POST""> {% csrf_token %}		<div id=""tb""> <textarea cols=""80"" rows=""20"" name=""data""></textarea></div><p><input type=""submit""></p>	</div>
+   # disp=o.render()
+    #return render_to_response(disp)
+    return render_to_response("one.html",RequestContext(request))
 def two(request):
      data = request.POST.get('data', '')
      if(data):
